f0849f60ff1d59b6767477d4c65296751e60bf51
kapii04 <119713755+kapii04@users.noreply.github.com>
2022-12-15 15:27:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
+API_ID = int(getenv("API_ID", "2176505")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1100725986").split()))
+OWNER_ID = int(getenv("OWNER_ID", "1100725986"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://kapiiuserbot:Brebes0705@cluster0.twayrkx.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "1829547354:AAEeITwyrCRHi4nkYMqiy06FhETwGsFgcwM")
 ALIVE_PIC = getenv("ALIVE_PIC")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
+REPO_URL = getenv("REPO_URL", "https://github.com/kapii04/kapiiuserbot")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkAFArMArAWOUsBzImAplK5V_9E7XhL6AIkYEX7prlWHJ8uknUS3TCtzoetEON7tqB5OFNcfStXUBTu2YKI5R3VEJ6SUe8ijW_9oA3uJHn6lHOLIw3rWq1G3UNAN2ZPb7V0urczrX8jTYUzkhy7CUsul4PHqMitZF-XNLklwOXRakqgf7dOi8eHwEiws96h8Dz3Vd8GEYgCtLfPKxhTo4RlYeYmMqnxo8QCm-UyFEN-vE0dGBl0XnYpQUxeAwsCaPrqymp57B8LgXR7atohpoSGMSccedY4pHMm2PWPrEHXdhD_bHFhdwfq0hPz9yh7EH4yrDxwzjc4vfb1FjS1jUDzgQAAAABBm77iAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
